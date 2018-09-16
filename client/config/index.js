4803f3024b43e5c7bc28cd2e9dd77b73a5753191
Alex Pepper <alexander.pepper@gmail.com>
2018-09-16 13:43:11 -0500

    update

--- client/config/index.js
+++ client/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/checklist/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
